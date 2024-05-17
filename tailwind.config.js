22f80288bb45342eae13575d75547c99d9030aca
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-05-17 16:46:11 +0200

    Finished adjusting colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,14 @@ export default {
     extend: {
       colors: {
         customHeaderDarkGg: 'rgb(51, 51, 51)',
-        customHeaderGreyBg: 'rgb(212, 208, 208)',
+        customHeaderGreyBg: 'rgb(213, 213, 213)',
         customBannerDivBg: 'rgba(51, 51, 51, 0.856)',
         customWhiteT: 'rgba(255, 255, 255, .76)',
         customTransparentWhite: 'rgba(255, 255, 255, 0.85)',
         customOrange: 'rgb(183, 149, 90)',
-        customDarkOrange: 'rgb(161,138,77)',
+        customDarkOrange: 'rgb(161, 138, 77)',
+        newsBackground: 'rgb(242, 242, 242)',
+        prevBtnBg: 'rgb(153, 153, 153)',
       },
     },
   },
