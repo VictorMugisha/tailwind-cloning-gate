9fa2be5475965dbe945dc8a021ec8d76ce074557
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-05-17 10:48:50 +0200

    Implemented banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ export default {
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customTransparentBlack: 'rgba(0, 0, 0, 0.65)', // Adjust the opacity as needed
+        customTransparentWhite: 'rgba(255, 255, 255, 0.5)', // Adjust the opacity as needed
+      },
+    },
   },
   plugins: [],
 }
